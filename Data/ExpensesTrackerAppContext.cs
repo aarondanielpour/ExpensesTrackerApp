909c7d8b20cb61bdc80d3a2c7069791fa6fbd25b
Aaron Danielpour <aarondanielpour@gmail.com>
2025-11-03 17:09:34 -0500

    First Commit: basic models and views

---
+++ Data/ExpensesTrackerAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpensesTrackerApp.Data
+{
+    public class ExpensesTrackerAppContext : DbContext
+    {
+        public ExpensesTrackerAppContext (DbContextOptions<ExpensesTrackerAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Expense> Expense { get; set; } = default!;
+    }
+}
---
+++ Models/Expense.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public enum ExpenseCategory
+{
+    Food,
+    Transportation,
+    Utilities,
+    Housing,
+    Healthcare,
+    Entertainment,
+    Education,
+    Miscellaneous
+}
+
+public class Expense
+{
+    public int Id { get; set; }
+
+    [Required]
+    public ExpenseCategory Category { get; set; } // e.g., Food, Utilities, Travel
+
+    [Required]
+    [Range(0.01, 1000000)]
+    public decimal Amount { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime Date { get; set; }
+
+    [StringLength(300)]
+    public string Description { get; set; }
+
+}
